94b1a0edadcd3a570b820b723e9c036ae8efeb70
Rémi COHEN SCALI <remi.cohen-scali@renault.com>
2022-11-10 03:58:18 +0100

    Added better comments in app module

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
+/**
+ * app.js
+ *
+ * Express javascript application renderred
+ */
 
+/**
+ * Load Modules Required
+ *
+ */
 // Require http-errors for handling www server errors
 var createError = require('http-errors');
 // Require NodeJS Express framework
@@ -20,6 +29,10 @@ var indexRouter = require('./routes/index');
 // Require routes users.js as users router (for resolving ~ notation)
 var usersRouter = require('./routes/users');
 
+/**
+ * App context
+ */
+
 var app = express();
 
 app.use(function (req, res, next) {
@@ -27,38 +40,65 @@ app.use(function (req, res, next) {
     next();
 });
 
-// Views engine setup
+/**
+ * Views engine setup
+ */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// Setup logger
+/**
+ // Setup logger
+ */
 app.use(logger('dev'));
+
+/**
+ // Middlewares
+ */
+
 // Setup body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 // Setup cookie parser middleware
 app.use(cookieParser());
+
+/**
+ // WWW Server setup
+ */
+
 // Setup static part of the server
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Setup pages handlers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+/**
+ // Error handler
+ */
+
+// catch 404 and forward
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
-// error handler
+/** error handler
+ */
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // set response status or 500 for any unknown condition
+    res.status(err.status || 500);
+    // Render error page
+    res.render('error');
 });
 
+/**
+ // Export app
+ */
 module.exports = app;
+
+// -*- mode: JavaScript; fill-column: 75; comment-column: 50; default-tab-width: 2; -*-
